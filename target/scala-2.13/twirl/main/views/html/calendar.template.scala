18dbb6afe96ee2c023eba7f1f686f2e3ad71c8cb
heyandremoreira <andre.moreira@iscte-iul.pt>
2022-12-21 17:16:29 +0000

    alterações às views, controlers, routes

---
+++ target/scala-2.13/twirl/main/views/html/calendar.template.scala
@@ -0,0 +1,65 @@
+
+package views.html
+
+import _root_.play.twirl.api.TwirlFeatureImports._
+import _root_.play.twirl.api.TwirlHelperImports._
+import _root_.play.twirl.api.Html
+import _root_.play.twirl.api.JavaScript
+import _root_.play.twirl.api.Txt
+import _root_.play.twirl.api.Xml
+import models._
+import controllers._
+import play.api.i18n._
+import views.html._
+import play.api.templates.PlayMagic._
+import java.lang._
+import java.util._
+import play.core.j.PlayMagicForJava._
+import play.mvc._
+import play.api.data.Field
+import play.data._
+import play.core.j.PlayFormsMagicForJava._
+import scala.jdk.CollectionConverters._
+
+object calendar extends _root_.play.twirl.api.BaseScalaTemplate[play.twirl.api.HtmlFormat.Appendable,_root_.play.twirl.api.Format[play.twirl.api.HtmlFormat.Appendable]](play.twirl.api.HtmlFormat) with _root_.play.twirl.api.Template0[play.twirl.api.HtmlFormat.Appendable] {
+
+  /**/
+  def apply():play.twirl.api.HtmlFormat.Appendable = {
+    _display_ {
+      {
+
+
+Seq[Any](format.raw/*1.1*/("""<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href=""""),_display_(/*5.39*/routes/*5.45*/.Assets.at("stylesheets/calendar.css")),format.raw/*5.83*/(""""/>
+        <script src=""""),_display_(/*6.23*/routes/*6.29*/.Assets.at("main.js")),format.raw/*6.50*/("""" defer></script>
+    </head>
+
+    <body>
+
+    </body>
+</html>"""))
+      }
+    }
+  }
+
+  def render(): play.twirl.api.HtmlFormat.Appendable = apply()
+
+  def f:(() => play.twirl.api.HtmlFormat.Appendable) = () => apply()
+
+  def ref: this.type = this
+
+}
+
+
+              /*
+                  -- GENERATED --
+                  SOURCE: app/views/calendar.scala.html
+                  HASH: ff06d44ddeff10fe3217b9baee04f25a2c1afefa
+                  MATRIX: 992->0|1204->186|1218->192|1276->230|1329->257|1343->263|1384->284
+                  LINES: 32->1|36->5|36->5|36->5|37->6|37->6|37->6
+                  -- GENERATED --
+              */
+          
