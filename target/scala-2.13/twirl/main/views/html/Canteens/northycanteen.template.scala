1274e39e62f3267eddc468fd8e586b1e5096913d
heyandremoreira <andre.moreira@iscte-iul.pt>
2022-12-21 10:49:29 +0000

    adição das views das cantinas routes homecontroller

---
+++ target/scala-2.13/twirl/main/views/html/Canteens/northycanteen.template.scala
@@ -0,0 +1,56 @@
+
+package views.html.Canteens
+
+import _root_.play.twirl.api.TwirlFeatureImports._
+import _root_.play.twirl.api.TwirlHelperImports._
+import _root_.play.twirl.api.Html
+import _root_.play.twirl.api.JavaScript
+import _root_.play.twirl.api.Txt
+import _root_.play.twirl.api.Xml
+import models._
+import controllers._
+import play.api.i18n._
+import views.html._
+import play.api.templates.PlayMagic._
+import java.lang._
+import java.util._
+import play.core.j.PlayMagicForJava._
+import play.mvc._
+import play.api.data.Field
+import play.data._
+import play.core.j.PlayFormsMagicForJava._
+import scala.jdk.CollectionConverters._
+
+object northycanteen extends _root_.play.twirl.api.BaseScalaTemplate[play.twirl.api.HtmlFormat.Appendable,_root_.play.twirl.api.Format[play.twirl.api.HtmlFormat.Appendable]](play.twirl.api.HtmlFormat) with _root_.play.twirl.api.Template0[play.twirl.api.HtmlFormat.Appendable] {
+
+  /**/
+  def apply():play.twirl.api.HtmlFormat.Appendable = {
+    _display_ {
+      {
+
+
+Seq[Any](_display_(/*1.2*/template(Array("stylesheets/canteen.css"))/*1.44*/{_display_(Seq[Any](format.raw/*1.45*/("""
+
+""")))}))
+      }
+    }
+  }
+
+  def render(): play.twirl.api.HtmlFormat.Appendable = apply()
+
+  def f:(() => play.twirl.api.HtmlFormat.Appendable) = () => apply()
+
+  def ref: this.type = this
+
+}
+
+
+              /*
+                  -- GENERATED --
+                  SOURCE: app/views/Canteens/northycanteen.scala.html
+                  HASH: 09705ea94562ae610e4bbdc690aa581a7a86c5ea
+                  MATRIX: 1006->1|1056->43|1094->44
+                  LINES: 32->1|32->1|32->1
+                  -- GENERATED --
+              */
+          
