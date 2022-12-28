84d5a37e977c39cd34a3ceb4660f9fc77e3a3837
heyandremoreira <andre.moreira@iscte-iul.pt>
2022-12-28 16:12:40 +0000

    re-organização das routes e controllers: - criação dos controllers Administrator, Manager e Student
    adição de um calendar
    criação das views userslist e authapproval

---
+++ target/scala-2.13/twirl/main/views/html/Settings/privacypolicy.template.scala
@@ -0,0 +1,56 @@
+
+package views.html.Settings
+
+import _root_.play.twirl.api.TwirlFeatureImports._
+import _root_.play.twirl.api.TwirlHelperImports._
+import _root_.play.twirl.api.Html
+import _root_.play.twirl.api.JavaScript
+import _root_.play.twirl.api.Txt
+import _root_.play.twirl.api.Xml
+import models._
+import controllers._
+import play.api.i18n._
+import views.html._
+import play.api.templates.PlayMagic._
+import java.lang._
+import java.util._
+import play.core.j.PlayMagicForJava._
+import play.mvc._
+import play.api.data.Field
+import play.data._
+import play.core.j.PlayFormsMagicForJava._
+import scala.jdk.CollectionConverters._
+
+object privacypolicy extends _root_.play.twirl.api.BaseScalaTemplate[play.twirl.api.HtmlFormat.Appendable,_root_.play.twirl.api.Format[play.twirl.api.HtmlFormat.Appendable]](play.twirl.api.HtmlFormat) with _root_.play.twirl.api.Template0[play.twirl.api.HtmlFormat.Appendable] {
+
+  /**/
+  def apply():play.twirl.api.HtmlFormat.Appendable = {
+    _display_ {
+      {
+
+
+Seq[Any](_display_(/*1.2*/template(Array("stylesheets/landing.css", "stylesheets/home.css"))/*1.68*/{_display_(Seq[Any](format.raw/*1.69*/("""
+
+""")))}))
+      }
+    }
+  }
+
+  def render(): play.twirl.api.HtmlFormat.Appendable = apply()
+
+  def f:(() => play.twirl.api.HtmlFormat.Appendable) = () => apply()
+
+  def ref: this.type = this
+
+}
+
+
+              /*
+                  -- GENERATED --
+                  SOURCE: app/views/Settings/privacypolicy.scala.html
+                  HASH: 0514a7b39b8b01e99f2320f19a1bcfffe0b89599
+                  MATRIX: 1006->1|1080->67|1118->68
+                  LINES: 32->1|32->1|32->1
+                  -- GENERATED --
+              */
+          
